8f776519112a4e9f6f977091dab3f5df3ee2cfd8
Almir <almir.tihak98@hotmail.com>
2020-07-19 23:38:40 +0200

    10 ispit POSTAVKA

---
+++ 10 ispitni 2019_06_25 (Postavka)/25_06_2019/RS1_Ispit/EntityModels/Ispit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RS1_Ispit_asp.net_core.EntityModels
+{
+    public class Ispit
+    {
+        public int ID { get; set; }
+
+        public int AngazovanID { get; set; }
+        public Angazovan Angazovan { get; set; }
+
+        public DateTime Datum { get; set; }
+        public string Napomena { get; set; }
+        public bool  Zakljucano { get; set; }
+    }
+}
---
+++ 10 ispitni 2019_06_25 (Postavka)/25_06_2019/RS1_Ispit/EntityModels/IspitDetalji.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RS1_Ispit_asp.net_core.EntityModels
+{
+    public class IspitDetalji
+    {
+        public int ID { get; set; }
+
+        public int IspitID { get; set; }
+        public Ispit Ispit { get; set; }
+
+        public int UpisGodineID { get; set; }
+        public UpisGodine UpisGodine { get; set; }
+
+        public bool Pristupio { get; set; }
+        public int? Ocjena { get; set; }
+    }
+}
